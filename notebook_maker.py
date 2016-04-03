7ad1b76658d9c23aa262d308320bed409af46de7
Michael <mik016@pitt.edu>
2016-04-03 14:26:35 -0400

    adding to get ready for test

--- notebook_maker.py
+++ notebook_maker.py
@@ -10,7 +10,7 @@ def run_notebook(notebook_file,outfile):
     subprocess.call(command)
 
 if __name__ == "__main__":
-    notebook_file = "AdaBoost_Hackathon.ipynb"
+    notebook_file = "AdaBoost_Hackathon-test.ipynb"
     outfile = "result_notebook.ipynb"
     run_notebook(notebook_file,outfile)
     create_html(outfile)
