023ff9da2eba2fd8939b059063dc338ae079537e
HaohanWang <haohanw@andrew.cmu.edu>
2016-04-03 12:28:43 -0400

    classifier

--- utility/KFoldCV.py
+++ utility/KFoldCV.py
@@ -3,14 +3,13 @@ import numpy as np
 import pandas as pd
 
 
-def kfoldCV(X, y, f, K):
+def kfoldCV(X, y, f, K, clf=None):
     X = X.as_matrix()
     y = y.as_matrix()
     kf = KFold(X.shape[0], n_folds=K)
-    s = np.zeros([1, 4])
+    s = np.zeros([5,])
     for train_index, test_index in kf:
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
-        s += f(pd.DataFrame(data=X_train), pd.DataFrame(data=y_train), pd.DataFrame(data=X_test),
-               pd.DataFrame(data=y_test))
+        s += f(X_train, y_train, X_test, y_test, clf=clf)
     return s / K
---
+++ utility/classifier.py
@@ -0,0 +1,62 @@
+from sklearn.ensemble import gradient_boosting, GradientBoostingClassifier
+import pandas as pd
+import numpy as np
+
+from KFoldCV import kfoldCV
+
+
+def evaluate(r, d):
+    tp = 0.
+    fp = 0.
+    tn = 0.
+    fn = 0.
+    for i in range(r.shape[0]):
+        if r[i] == d[i]:
+            if d[i] == 1:
+                tp += 1
+            else:
+                fp += 1
+        else:
+            if d[i] == 1:
+                fn +=1
+            else:
+                tn +=1
+    k = np.array([tp, fp, tn, fn, (tp+tn)/r.shape[0]])
+    return k
+
+
+def clffit(t1, d1, t2, d2, clf):
+    clf.fit(t1, d1)
+    r = clf.predict(t2)
+    return evaluate(r, d2)
+
+
+def classify(data, labels):
+    lrs = [0.1, 0.5, 1]
+    estimateNum = [50, 100, 200]
+    depths = [3, 4, 5]
+    blr = 0
+    be = 0
+    bd = 0
+    ba = -np.inf
+    for lr in lrs:
+        for en in estimateNum:
+            for d in depths:
+                clf = GradientBoostingClassifier(learning_rate=lr, n_estimators=en, max_depth=d)
+                s = kfoldCV(data, labels, clffit, 5, clf)
+                if s[-1] > ba:
+                    ba = s[-1]
+                    blr = lr
+                    be = en
+                    bd = d
+    clf = GradientBoostingClassifier(learning_rate=blr, n_estimators=be, max_depth=bd)
+    clf.fit(data.as_matrix(), labels.as_matrix())
+    return ba, clf.feature_importances_
+
+
+if __name__ == '__main__':
+    df = pd.DataFrame.from_csv('../data/data_.csv')
+    labels = pd.DataFrame(data=[np.random.randint(0, 2) for i in range(10)])
+    accuracy, feature_importance = classify(df, labels)
+    print accuracy
+    print feature_importance
